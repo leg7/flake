696a9b85ffa962c0d27b3bcba7a82c38aecfd72f
leg7 <dev@leonardgomez.xyz>
2023-06-08 13:58:36 +0200

    Use new services and programs

--- modules/base.nix
+++ modules/base.nix
@@ -3,50 +3,6 @@
 { pkgs, ... }: {
   imports = [ ./fdeImpermanence.nix ];
 
-  environment = {
-    sessionVariables = rec {
-      EDITOR = "vim";
-      PAGER = "less";
-      BROWSER = "firefox";
-
-      XDG_CACHE_HOME  = "$HOME/.local/cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME   = "$HOME/.local/share";
-      XDG_STATE_HOME  = "$HOME/.local/state";
-      PATH = [ "$HOME/.local/bin" ];
-
-      HISTFILE              = "$XDG_STATE_HOME/bash/history";
-      VIMINIT               = "set nocp | source $XDG_CONFIG_HOME/vim/vimrc";
-      GNUPGHOME             = "$XDG_DATA_HOME/gnupg";
-      WGETRC                = "$XDG_CONFIG_HOME/wget/wgetrc";
-      GOPATH                = "$XDG_DATA_HOME/go";
-      GOMODCACHE            = "$XDG_CACHE_HOME/go/mod";
-      RUSTUP_HOME           = "$XDG_DATA_HOME/rustup";
-      CARGO_HOME            = "$XDG_DATA_HOME/cargo";
-      STACK_ROOT            = "$XDG_DATA_HOME/stack";
-      NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/npmrc";
-      NUGET_PACKAGES        = "$XDG_CACHE_HOME/NuGetPackages";
-    };
-
-    systemPackages = with pkgs; [
-      xdg-utils xdg-user-dirs xdg-ninja
-      p7zip
-      keyd
-      vim
-      vimv
-      wget
-      fzf
-      tree
-      git
-      bottom
-      shellcheck
-    ];
-
-    binsh = "${pkgs.dash}/bin/dash";
-  };
-
-  programs.fish.enable = true;
-
   users = {
     mutableUsers = false;
 
@@ -78,6 +34,62 @@
     sudo.enable = false;
   };
 
+  environment = {
+    sessionVariables = rec {
+      EDITOR = "vim";
+      PAGER = "less";
+      BROWSER = "firefox";
+
+      XDG_CACHE_HOME  = "$HOME/.local/cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME   = "$HOME/.local/share";
+      XDG_STATE_HOME  = "$HOME/.local/state";
+      PATH = [ "$HOME/.local/bin" ];
+
+      HISTFILE              = "$XDG_STATE_HOME/bash/history";
+      VIMINIT               = "set nocp | source $XDG_CONFIG_HOME/vim/vimrc";
+      GNUPGHOME             = "$XDG_DATA_HOME/gnupg";
+      WGETRC                = "$XDG_CONFIG_HOME/wget/wgetrc";
+      GOPATH                = "$XDG_DATA_HOME/go";
+      GOMODCACHE            = "$XDG_CACHE_HOME/go/mod";
+      RUSTUP_HOME           = "$XDG_DATA_HOME/rustup";
+      CARGO_HOME            = "$XDG_DATA_HOME/cargo";
+      STACK_ROOT            = "$XDG_DATA_HOME/stack";
+      NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/npmrc";
+      NUGET_PACKAGES        = "$XDG_CACHE_HOME/NuGetPackages";
+    };
+
+    systemPackages = with pkgs; [
+      xdg-utils xdg-user-dirs xdg-ninja
+      p7zip
+      vim
+      vimv
+      wget
+      tree
+      bottom
+      shellcheck
+    ];
+
+    binsh = "${pkgs.dash}/bin/dash";
+  };
+
+  programs = {
+    fish.enable = true;
+
+    fzf = {
+      fuzzyCompletion = true;
+      keybindings = true;
+    };
+
+    git = {
+      enable = true;
+      config.init = {
+          defaultBranch = "main";
+      };
+    };
+  };
+
+# -- Networking --
   systemd.network = {
     enable = true;
 
