4a58ab59417189e6d7d6e19d9b5d8db1a2b92775
leg7 <dev@leonardgomez.xyz>
2023-06-18 11:20:19 +0200

    Make install script simpler

--- install.sh
+++ install.sh
@@ -35,8 +35,18 @@ else
 	sysPartition="$disk"2
 fi
 
-fs()
-{
+
+if [ "$1" = "secureboot" ]; then
+	# Partition disks
+	parted "$disk" mklabel gpt
+	parted "$disk" mkpart primary fat32 0% 512MiB set 1 esp on
+	parted "$disk" mkpart primary 512MiB 100%
+
+    # Encrypt disk (you might need to adjust the memory needed)
+    cryptsetup luksFormat --type luks2 -i 5000 --pbkdf argon2id --pbkdf-memory 4000000 --hash sha512 "$sysPartition"
+    cryptsetup open "$sysPartition" cryptLvm
+    cryptsetup config "$sysPartition" --label cryptLvm
+
     # LVM
     pvcreate /dev/mapper/cryptLvm
     vgcreate pool /dev/mapper/cryptLvm
@@ -56,31 +66,38 @@ fs()
     mount -m -o compress_algorithm=zstd,compress_chksum,atgc,gc_merge,lazytime /dev/pool/nix /mnt/nix
     mount -m "$efiPartition" /mnt/boot
     mkdir -p /mnt/nix/persistent/etc/ # for impermanence to work
-}
-
-# Partition disks
-parted "$disk" mklabel gpt
-parted "$disk" mkpart primary fat32 0% 512MiB set 1 esp on
-parted "$disk" mkpart primary 512MiB 100%
-
-if [ "$1" = "secureboot" ]; then
-    # Encrypt disk (you might need to adjust the memory needed)
-    cryptsetup luksFormat --type luks2 -i 5000 --pbkdf argon2id --pbkdf-memory 4000000 --hash sha512 "$sysPartition"
-    cryptsetup open "$sysPartition" cryptLvm
-    cryptsetup config "$sysPartition" --label cryptLvm
-
-    fs
 
     nix-shell -p sbctl --run 'sbctl create-keys'
     cp -r /etc/secureboot /mnt/nix/persistent/etc/secureboot
     cp -r /etc/secureboot /mnt/etc/
 elif [ "$1" = "grub" ]; then
+	# Use a smaller boot partition
+	parted "$disk" mklabel gpt
+	parted "$disk" mkpart primary fat32 0% 128MiB set 1 esp on
+	parted "$disk" mkpart primary 128MiB 100%
+
+	# Use luks1 and a keyfile
     cryptsetup luksFormat --type luks1 -i 5000 --hash sha512 "$sysPartition"
     dd if=/dev/urandom of=./keyfile.bin bs=1024 count=4
     cryptsetup luksAddKey "$sysPartition" keyfile.bin
     cryptsetup open "$sysPartition" cryptLvm -d keyfile.bin
 
-    fs
+    pvcreate /dev/mapper/cryptLvm
+    vgcreate pool /dev/mapper/cryptLvm
+
+    size="$(awk 'NR==1 {print $2 * 1.5 / 1024 / 1024}' /proc/meminfo)" # 1.5x the total ram
+    lvcreate -L "$size"G -n swap pool
+    lvcreate -l 100%FREE -n nix pool
+
+    mkfs.fat -n ESP -F32 "$efiPartition"
+    mkfs.f2fs -l nix -O extra_attr,inode_checksum,sb_checksum,compression /dev/pool/nix
+    mkswap /dev/pool/swap
+    swapon /dev/pool/swap
+
+    mount -m -o size=10G -t tmpfs none /mnt
+    mount -m -o compress_algorithm=zstd,compress_chksum,atgc,gc_merge,lazytime /dev/pool/nix /mnt/nix
+    mount -m "$efiPartition" /mnt/boot/efi # Mount to boot/efi
+    mkdir -p /mnt/nix/persistent/etc/
 
     # Move the key
     mkdir -p /mnt/nix/persistent/etc/secrets/initrd/
