1389d8825b36c20263cca2d25ce2c2d5bf6dbafb
leg7 <dev@leonardgomez.xyz>
2023-07-08 11:22:46 +0200

    Automate backups

--- modules/backups.nix
+++ modules/backups.nix
@@ -1,5 +1,5 @@
 # An extra encrypted luks volume to keep backups that will be decrypted at boot
-{ ... }: {
+{ pkgs, ... }: {
   boot.initrd.luks.devices.cryptBackup = {
     device = "/dev/disk/by-label/cryptBackup";
     allowDiscards = true;
@@ -9,5 +9,32 @@
       device = "/dev/disk/by-label/backup";
       fsType = "f2fs";
       options = [ "noauto" "compress_algorithm=zstd" "compress_chksum" "atgc" "gc_merge" "lazytime" ];
+  };
+
+  systemd = {
+    services."backup" = {
+	  script = ''
+	    backupDir=/nix/persistent/backup
+	    home=/nix/persistent/home/user
+
+	    ${pkgs.util-linux}/bin/mount "$backupDir"
+		${pkgs.gnutar}/bin/tar cvf "$backupDir"/"$(date +"%F_%H-%M-%S")".tar $home/books $home/documents $home/pics $home/vids $home/audio &&
+		${pkgs.findutils}/bin/find "$backupDir" -mtime +5 -delete
+	    ${pkgs.util-linux}/bin/umount "$backupDir"
+	  '';
+	  serviceConfig = {
+	    Type = "oneshot";
+	    User = "root";
+      };
+	};
+
+	timers."backup" = {
+	  wantedBy = [ "timers.target" ];
+	  timerConfig = {
+	    OnCalendar = "daily";
+		Persistent = true;
+		unit = "backup.service";
+	  };
+	};
   };
 }
