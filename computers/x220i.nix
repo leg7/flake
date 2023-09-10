ef99b8fce71af5bbafbd286b6ef08a12b8c3fff1
leg7 <dev@leonardgomez.xyz>
2023-09-10 23:58:39 +0200

    Set the x220i to french and systemd

--- computers/x220i.nix
+++ computers/x220i.nix
@@ -1,9 +1,11 @@
 { pkgs, lib, config, ... }: {
-  imports = [ ./modules/boot/grub.nix ./modules/systems/gnome.nix ];
+  imports = [ ./modules/boot/systemd.nix ./modules/systems/gnome.nix ];
 
   system.stateVersion = "23.11";
   nixpkgs.config.allowUnfree = true;
   time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  console.keyMap = "fr";
 
   # Use cryptsetup luksUUID to find the uuid of the device
   # boot.initrd.luks.devices.cryptLvm.device = lib.mkOverride 50 "/dev/disk/by-uuid/";
