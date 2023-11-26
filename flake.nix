797840f335971112a5a1b9c833355ae489d0dc2c
leg7 <dev@leonardgomez.xyz>
2023-11-26 21:00:31 +0100

    Switch back to nixpkgs master branch

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
