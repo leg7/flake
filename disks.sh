66e525794a801c5c739783ad9053404fb892a2e9
leg7 <dev@leonardgomez.xyz>
2024-03-04 00:11:29 +0100

    secureboot: move keys to the correct path in disks.sh

--- disks.sh
+++ disks.sh
@@ -2,7 +2,7 @@
 
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko -f /home/nixos/flake/#testing
 nix-shell -p sbctl --run 'sbctl create-keys'
-mkdir -p /mnt/nix/persistent/etc/secureboot
-cp -r /etc/secureboot /mnt/nix/persistent/etc/secureboot
+mkdir -p /mnt/persistent/etc/secureboot
+cp -r /etc/secureboot /mnt/persistent/etc/secureboot
 cp -r /etc/secureboot /mnt/etc/
 nixos-install --no-root-passwd --flake /home/nixos/flake#testing --cores 0 --root /mnt
