58a6ba3879bce69b895e12c670cbdbda25eec5ac
leg7 <dev@leonardgomez.xyz>
2024-03-04 12:12:27 +0100

    secureboot: fix disks.sh to move keys correctly

--- disks.sh
+++ disks.sh
@@ -3,6 +3,6 @@
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko -f /home/nixos/flake/#testing
 nix-shell -p sbctl --run 'sbctl create-keys'
 mkdir -p /mnt/persistent/etc/secureboot
-cp -r /etc/secureboot /mnt/persistent/etc/secureboot
+cp -r /etc/secureboot /mnt/persistent/etc/
 cp -r /etc/secureboot /mnt/etc/
 nixos-install --no-root-passwd --flake /home/nixos/flake#testing --cores 0 --root /mnt
