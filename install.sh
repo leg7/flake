7527232ed099e60466a7a212f821d598150de64f
leg7 <dev@leonardgomez.xyz>
2024-03-10 12:51:18 +0100

    install.sh: pass host to install as $1 and secureboot if needed

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko -f /home/nixos/flake/#testing
-nix-shell -p sbctl --run 'sbctl create-keys'
-mkdir -p /mnt/persistent/etc/secureboot
-cp -r /etc/secureboot /mnt/persistent/etc/
-cp -r /etc/secureboot /mnt/etc/
-nixos-install --no-root-passwd --flake /home/nixos/flake#testing --cores 0 --root /mnt
+if [ "$1" = "secureboot" ]; then
+	nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko -f /home/nixos/flake/#"$2"
+	nix-shell -p sbctl --run 'sbctl create-keys'
+	mkdir -p /mnt/persistent/etc/secureboot
+	cp -r /etc/secureboot /mnt/persistent/etc/
+	cp -r /etc/secureboot /mnt/etc/
+	nixos-install --no-root-passwd --flake /home/nixos/flake#"$2" --cores 0 --root /mnt
+else
+	nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko -f /home/nixos/flake/#"$1"
+	nixos-install --no-root-passwd --flake /home/nixos/flake#"$1" --cores 0 --root /mnt
+fi
