ead54dafb93b38c58eb389828a1ec14065ecb70e
leg7 <dev@leonardgomez.xyz>
2024-08-17 11:55:13 +0200

    flake: use stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     impermanence.url = "github:nix-community/impermanence";
@@ -65,6 +65,7 @@
           lanzaboote.nixosModules.lanzaboote
           impermanence.nixosModules.impermanence
           disko.nixosModules.disko
+          ({pkgs, config, ...}: { nixpkgs.overlays = [ overlay-unstable ]; })
           ./computers/rpi4.nix
           home-manager.nixosModules.home-manager
           {
