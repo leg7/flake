22cd05391b67c41d32ae0decdbe7e3935f501f2f
leg7 <dev@leonardgomez.xyz>
2025-02-11 10:22:48 +0100

    install: auto-accept options

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [ "$1" = "secureboot" ]; then
 	mkdir -p /mnt/persistent/etc/secureboot
 	cp -r /etc/secureboot /mnt/persistent/etc/
 	cp -r /etc/secureboot /mnt/etc/
-	nixos-install --no-root-passwd --flake /home/nixos/flake#"$2" --cores 0 --root /mnt
+	nixos-install --no-root-passwd --flake /home/nixos/flake#"$2" --cores 0 --root /mnt --accept-flake-config
 elif [ "$1" = "rpi4-firmware" ]; then
 	cd /mnt/boot || exit
 	nix-shell -p wget --run 'wget https://github.com/pftf/RPi4/releases/download/v1.37/RPi4_UEFI_Firmware_v1.37.zip'
@@ -14,5 +14,5 @@ elif [ "$1" = "rpi4-firmware" ]; then
 	rm README.md RPi4_UEFI_Firmware_v1.37.zip
 else
 	nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko -f /home/nixos/flake/#"$1"
-	nixos-install --no-root-passwd --flake /home/nixos/flake#"$1" --cores 0 --root /mnt
+	nixos-install --no-root-passwd --flake /home/nixos/flake#"$1" --cores 0 --root /mnt --accept-flake-config
 fi
