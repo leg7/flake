03025562290705e9c4ded5721d6e7cdb377c7943
leg7 <dev@leonardgomez.xyz>
2025-05-15 16:28:48 +0200

    base: Add git log alias

--- systems/base/base.nix
+++ systems/base/base.nix
@@ -110,6 +110,7 @@
         gc = "git commit";
         gca = "git commit --amend";
         gs = "git status";
+        gl = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
       };
 
       histSize = 10000;
